9a56b7a6d8b9c734590cff7dcdec9e5cd88d784a
kosta <konstantin@bitracks.com>
2020-10-30 10:16:08 +0100

    migrations initalized

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import { Post } from "./entities/Post";
+import mikroConfig from './mikro-orm.config';
 
 const main = async () => {
 
-  
-  const orm = await MikroORM.init({
-    entities:[],
-    dbName: 'lireddit',
-    user: '',
-    password: '',
-    type: 'postgresql',
-    debug: !__prod__,
-  });
-  
-  console.log("hhh");
-  
+  const orm = await MikroORM.init(mikroConfig);
+  const post = orm.em.create(Post, {title: "My first post"});
+  await orm.em.persistAndFlush(post);
 }
+
+main().catch(err => {
+  console.log(err);
+});
